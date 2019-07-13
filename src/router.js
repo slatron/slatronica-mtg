d17930c9495297a6af7633c463fbe75219ab8000
Mike Slater <slatron@gmail.com>
2019-07-13 10:45:46 -0500

    add path to default app export

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Gallery from './views/Gallery.vue'
 
 Vue.use(Router)
 
@@ -12,7 +11,7 @@ export default new Router({
     {
       path: '/',
       name: 'gallery',
-      component: Gallery
+      component: () => import('./views/Gallery.vue')
     },
     {
       path: '/about',
