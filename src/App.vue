5aeb744fa290888e888615ad85fb11d1b461f4f7
Mike Slater <slatron@gmail.com>
2019-07-15 21:54:22 -0500

    add css for larger screens

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,12 @@
       <router-link to="/about">About</router-link>
     </div>
     -->
-    <h1>Slatronica Alters</h1>
-    <router-view/>
+    <header>
+      <h1>Slatronica Alters</h1>
+    </header>
+    <main>
+      <router-view/>
+    </main>
     <footer>
       <p>alters by mike slater</p>
     </footer>
@@ -20,15 +24,12 @@ body {
   background: #000;
   text-align: center;
 }
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #487cb1;
-}
-
-h1 {
-  font-size: 48px;
 }
 
 img {
@@ -37,9 +38,54 @@ img {
 
 #nav {}
 
-footer p {
-  font-size: 10px;
-  margin-right: 2em;
-  text-align: right;
+header {
+  background: #000;
+  position: fixed;
+  top: 0;
+  height: 36px;
+  width: 100%;
+  z-index: 1000;
+
+  h1 {
+    font-size: 24px;
+    margin: 0;
+    padding: 0;
+  }
 }
+
+@media (min-width: 520px) {
+  header {
+    height: 52px;
+    h1 {
+      font-size: 36px;
+    }
+  }
+}
+
+main {
+  padding-top: 36px;
+}
+
+@media (min-width: 520px) {
+  main {
+    padding-top: 52px;
+  }
+}
+
+footer {
+  background: #000;
+  font-size: 10px;
+  text-align: right;
+  position: fixed;
+  bottom: 0;
+  height: 15px;
+  width: 100%;
+  z-index: 1000;
+
+  p {
+    margin: 0 2em 0;
+    padding: 0;
+  }
+}
+
 </style>
