16d47272bf8d28fba6343b74cdf0cc077ee9cf70
Mike Slater <slatron@gmail.com>
2019-10-30 22:38:51 -0500

    add files

---
+++ src/components/gallery/AddAlter.vue
@@ -0,0 +1,17 @@
+<template>
+  <div
+    v-show="false"
+    class="absolute top-0 right-0 bottom-0 left-0 background-color-black text-white">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'addAlter',
+  computed: {
+    open_form () {
+      return this.$store.state.add_click
+    }
+  }
+}
+</script>
