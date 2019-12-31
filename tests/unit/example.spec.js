6c0d477439db844581904551eb465f0ee016c119
Mike Slater <slatron@gmail.com>
2019-12-30 22:18:43 -0600

    installs jest for unit tests

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,16 +1,9 @@
-import { expect } from 'chai'
-import { shallowMount } from '@vue/test-utils'
-import GalleryCard from '@/components/GalleryCard.vue'
+import { mount } from '@vue/test-utils'
+import IconBase from '@/components/common/IconBase'
 
-describe('GalleryCard.vue', () => {
-  it('Calls API with id passed on cardData', () => {
-    let cardData = { id: '3c02b8ee-84cb-44cb-ba14-9e725a9d03ee' }
-    const wrapper = shallowMount(GalleryCard, {
-      propsData: { cardData }
-    })
-    wrapper.vm.$nextTick(() => {
-      expect(wrapper.vm.title).to.equal('Ponder')
-      done()
-    })
+describe('IconBase', () => {
+  test('IconBase is a Vue instance', () => {
+    const wrapper = mount(IconBase)
+    expect(wrapper.isVueInstance()).toBeTruthy()
   })
 })
